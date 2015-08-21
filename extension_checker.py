1c2a787928b43a587e099151d2595744bdd72383
Pavel Kravetskiy <pkravetskiy@gmail.com>
2015-08-21 14:42:25 +0200

    implemented logging added extensions permissions check added info about known permissions

--- extension_checker.py
+++ extension_checker.py
@@ -9,6 +9,7 @@ import json
 import argparse
 import platform
 import sys
+import logging
 
 
 def locale_lookup(path, default_locale, key):
@@ -59,20 +60,33 @@ def extract_data(path):
     with open(manifest_name) as fh:
         data = json.load(fh)
         i18n(path, data)
-        return data
+    return data
 
 
-def logtofile(txt="\n"):
-    with open("logext.txt", "a") as f:
-        f.write(txt)
-    f.close()
+def initializeMultiLogging():
+    logging.basicConfig(filename="logext.log",
+                        filemode="w",
+                        level=logging.INFO,
+                        format="%(message)s")
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(message)s")
+    console.setFormatter(formatter)
+    logging.getLogger("").addHandler(console)
 
 
 def load_json(filename):
-    if filename is None:
-        return None
-    with open(filename) as fh:
-        return json.load(fh)
+    if filename and os.path.isfile(filename):
+        with open(filename) as fh:
+            return json.load(fh)
+    return None
+
+
+def list_of_permissions(data):
+    lst = []
+    if data and "permissions" in data:
+        lst = [permission for permission in data["permissions"]]
+    return lst
 
 
 def check_ext_list(path, must_file=None, can_file=None):
@@ -88,7 +102,6 @@ def check_ext_list(path, must_file=None, can_file=None):
     must = load_json(must_file)
     can = load_json(can_file)
 
-    not_in_can = []
     res = True
     if args.all_profiles:
         profiles = get_profiles()
@@ -101,39 +114,33 @@ def check_ext_list(path, must_file=None, can_file=None):
         path = os.path.join(path_bak, profile, "Extensions")
         if not os.path.isdir(path):
             continue
-        print(profile.center(60, "="))
-        logtofile("\n" + profile.center(60, "="))
+        not_in_can = []
+        logging.info("\n" + profile.center(60, "="))
         for ext in os.listdir(path):
             if ext == "Temp":
                 continue
-            print(ext)
-            logtofile("\nExtension ID:  " + ext)
+            logging.info(ext)
             ext_path = os.path.join(path, ext)
             for ver in os.listdir(ext_path):
                 ver_path = os.path.join(ext_path, ver)
                 data = extract_data(ver_path)
-                print("\t Name: %s" % data.get("name"))
-                logtofile("\nName: %s" % data.get("name"))
-                logtofile("     Version: %s   " % data.get("version"))
-                print("\t Version: %s" % data.get("version"))
-                print("\t Description: %s" % data.get("description"))
-                logtofile("Description: %s" % data.get("description"))
+                logging.info("\tName: %s\n\tVersion: %s\n\tDescription: %s\n\tPermissions: %s" %
+                             (data.get("name"), data.get("version"), data.get("description"),
+                              ", ".join(list_of_permissions(data))))
                 if can is not None:
                     if data["name"] not in can:
-                        print("  ERROR: this extension is not allowed or might be unwanted! ")
-                        logtofile("      ERROR: this extension is not allowed or might be unwanted!")
-                        not_in_can.append(data)
+                        logging.warning("WARNING: this extension is not allowed or might be unwanted!")
+                        not_in_can.append(data["name"])
                         res = False
                 if must is not None:
                     if data.get("name") in must:
                         must.remove(data.get("name"))
-                print("\n")
-                logtofile()
+                logging.info("\n")
+        if not_in_can:
+            logging.warning("There are not allowed or unwanted extensions:\n%s\n" % ", ".join(not_in_can))
         if must is not None and len(must) > 0:
-            print("There are missing extensions:")
-            print(", ".join(must))
-            logtofile("ERROR!   There are missing extensions:  ")
-            logtofile("> ".join(must))
+            logging.info("There are missing extensions:")
+            logging.info(", ".join(must))
             res = False
     return res
 
@@ -148,19 +155,24 @@ def get_profiles(only_current=False):
         else:
             profiles = [profile for profile in load_json("Local State")["profile"]["info_cache"]]
     except:
-        print("No profiles found")
+        logging.info("No profiles found")
         return profiles
     finally:
         os.chdir(old_dir)
     if profiles:
-        print("Profiles to check: %s" % ", ".join(profiles))
-        logtofile("Profiles to check: %s" % ", ".join(profiles))
+        logging.info("Profiles to check: %s" % ", ".join(profiles))
     return profiles
 
 
+def Main(args):
+    initializeMultiLogging()
+
+    if check_ext_list(args.config_path, args.must_extensions, args.can_extensions):
+        return 1
+    return 0
+
+
 if __name__ == "__main__":
-    if os.path.isfile("logext.txt"):
-        os.remove("logext.txt")
     parser = argparse.ArgumentParser(description="Checks the extensions installed in Chrome/Chromium/Avira Browser")
     if "windows" in platform.system().lower():
         parser.add_argument("--config_path", help="The browser config path",
@@ -182,6 +194,64 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    if not check_ext_list(args.config_path, args.must_extensions, args.can_extensions):
-        exit(1)
-    exit(0)
+    sys.exit(Main(args))
+
+
+permissins_info = {
+    "activeTab": "Access to the currently active tab when the user invokes the extension - for example by clicking its browser action. Access to the tab lasts until the tab is navigated or closed.",
+    "alarms": "Scheduling code to run periodically or at a specified time in the future.",
+    "audioModem": "",
+    "background": "Make Browser start up early and and shut down late, so that apps and extensions can have a longer life. Also makes Browser continue running (even after its last window is closed) until the user explicitly quits Browser.",
+    "bookmarks": "Access to the chrome bookmarks, so that it can create, organize, and manipulate bookmarks.",
+    "browsingData": "Remove browsing data from a user's local profile.",
+    "clipboardRead": "Access data you copy and paste.",
+    "clipboardWrite": "Write to clipboard.",
+    "contentSettings": "Manipulate settings that specify whether websites can use features such as cookies, JavaScript, plugins, geolocation, microphone, camera etc.",
+    "contextMenus": "Add items to Browser's context menu",
+    "cookies": "Query and modify cookies, and to be notified when they change.",
+    "copresence": "Communicate with other nearby devices using Google's copresence service.",
+    "debugger": "Read and modify all your data on all websites you visit. Access to instrument network interaction, debug JavaScript, mutate the DOM and CSS, etc.",
+    "declarativeContent": "Take actions depending on the content of a page, without requiring permission to read the page's content.",
+    "declarativeWebRequest": "Intercept, block, or modify requests in-flight.",
+    "desktopCapture": "Capture content of screen, individual windows or tabs.",
+    "dns": "",
+    "downloads": "Programmatically initiate, monitor, manipulate, and search for downloads.",
+    "experimental": "Different experimental features",
+    "fontSettings": "Manage Chrome's font settings.",
+    "gcm": "Enable apps and extensions to send and receive messages through the Google Cloud Messaging Service.",
+    "geolocation": "Access to geographical location information associated with the hosting device. Detect your physical location.",
+    "history": "Interact with the browser's record of visited pages.",
+    "identity": "Get OAuth2 access tokens.",
+    "idle": "Detect when the machine's idle state changes.",
+    "idltest": "",
+    "location": "Retrieve the geographic location of the host machine.",
+    "management": "Manage the list of extensions/apps and themes that are installed or running.",
+    "mdns": "Discover devices on your local network",
+    "nativeMessaging": "Exchange messages with registered native applications.",
+    "notificationProvider": "",
+    "notifications": "Create rich notifications using templates and show these notifications to users in the system tray.",
+    "pageCapture": "Save a tab as MHTML. Read and modify all your data on all websites you visit.",
+    "platformKeys": "Access to client certificates managed by the platform. If the user or policy grants the permission, an extension can use such a certficate in its custom authentication protocol. E.g. this allows usage of platform managed certificates in third party VPNs.",
+    "plugin": "Read and modify all your data on your computer and the websites you visit.",
+    "power": "Override the system's power management features.",
+    "printerProvider": "Exposes events used by print manager to query printers controlled by extensions, to query their capabilities and to submit print jobs to these printers.",
+    "privacy": "Control usage of the features in Chrome that can affect a user's privacy. Manipulate privacy-related settings.",
+    "processes": "Interact with the browser's processes.",
+    "proxy": "Manage Chrome's proxy settings. Read and modify all your data on all websites you visit.",
+    "sessions": "Query and restore tabs and windows from a browsing session.",
+    "signedInDevices": "Get a list of devices signed into chrome with the same account as the current profile.",
+    "storage": "Store, retrieve, and track changes to user data.",
+    "system.cpu": "Query information about CPU.",
+    "system.display": "Query information about display.",
+    "system.memory": "Query information about physical memory.",
+    "system.storage": "Query storage device information and be notified when a removable storage device is attached and detached.",
+    "tabCapture": "Interact with tab media streams",
+    "tabs": "Get additional privileged tab information. Access your browsing activity.",
+    "topSites": "Access to the top sites that are displayed on the new tab page. Read and modify your browsing history.",
+    "tts": "Play synthesized text-to-speech (TTS).",
+    "ttsEngine": "Implement a text-to-speech(TTS) engine using an extension. Access all text spoken using synthesized speech.",
+    "unlimitedStorage": "Provide an unlimited quota for storing HTML5 client-side data, such as databases and local storage files. Without this permission, the extension or app is limited to 5 MB of local storage.",
+    "webNavigation": "Receive notifications about the status of navigation requests in-flight. Access your browsing activity.",
+    "webRequest": "Observe and analyze traffic and to intercept, block, or modify requests in-flight.",
+    "webRequestBlocking": "Observe and analyze traffic and to intercept, block, or modify requests in-flight."
+}
